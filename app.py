7bb745d6e64540602c6cbf0ad683a19a86bf493f
Kabure <ferreira_master@hotmail.com>
2020-07-02 23:31:55 -0300

    trakcing-access

--- app.py
+++ app.py
@@ -38,6 +38,27 @@ app = dash.Dash(
 
 app.title = "trich.ai portfolio"
 
+
+app.index_string = """<!DOCTYPE html>
+<html>
+    <head>
+        {%metas%}
+        <title>{%title%}</title>
+        {%favicon%}
+        {%css%}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-1DYNFQVF7N"></script>
+    </head>
+    <body>
+        {%app_entry%}
+        <footer>
+            {%config%}
+            {%scripts%}
+            {%renderer%}
+        </footer>
+    </body>
+</html>"""
+
+
 server = app.server
 
 app.layout = Homepage()
